1a9bda8b65b736c6c14e6a0e6be09cee5a41bf21
ravinder90nath <ravi89nath@gmail.com>
2021-11-28 00:34:56 +0530

    New file update

---
+++ Program1/JavaFirstProgram/src/SwitchCase.java
@@ -0,0 +1,25 @@
+
+public class SwitchCase {
+	public static void main(String[] argh) {
+		
+		int num = 4;
+		
+		switch(num){
+			case 1:
+				System.out.println("neither prime nor composite");
+				break;
+				
+			case 2:
+				System.out.println("no is prime");
+				break;
+				
+			case 4:
+				System.out.println("no is composite");
+				break;
+				
+			default:
+				System.out.println("Number dos not exist");
+				
+		}
+	}
+}
